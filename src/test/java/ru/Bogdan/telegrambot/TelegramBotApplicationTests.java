85c419c0783b93e45648e830bf25346227304ecc
Bogdanshumeiko <bsshumeiko@gmail.com>
2023-07-17 00:43:18 +0300

    practical_task_2

---
+++ src/test/java/ru/Bogdan/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Bogdan.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
